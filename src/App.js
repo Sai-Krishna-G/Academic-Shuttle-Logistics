a20f11c8b36985d56cc6f5e22c3d07394cf740eb
G Sai Krishna <ugs21108_it.krishna@cbit.org.in>
2023-11-18 22:11:10 +0530

    booking

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router,  Route, Routes } from "react-router-dom";
 import Login from './Pages/Login&Signup/Login';
 import Signup from './Pages/Login&Signup/Signup';
-import Home from './Pages/Home';
+//import Home from './Pages/Home';
 import NavBar from './Pages/Navbar/Navbar';
 import Booking from './Pages/Booking';
+import {View,Text} from 'react-native';
+
 
 function App() {
   return (
@@ -14,7 +16,7 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Login />} />
-            <Route path="/Home" element={<Home />} />
+         
             <Route path="/Signup" element={<Signup />} />
             <Route path="/Booking" element={<Booking />} />
           </Routes>
