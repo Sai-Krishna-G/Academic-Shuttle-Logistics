fb71fd1b3464cf84bd384d3d3f0f7d31cb04de38
Abhinav-Keesari <keesari.abhinav5940@gmail.com>
2023-11-21 10:05:08 +0530

    Fourth commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router,  Route, Routes } from "react-router-dom";
 import Login from './Pages/Login&Signup/Login';
 import Signup from './Pages/Login&Signup/Signup';
-import Home from './Pages/Home';
-import NavBar from './Pages/Navbar/Navbar';
-import Booking from './Pages/Booking';
+import Home from './Pages/Home/Home.js';
+import NavBar from './Pages/Nav-1/Navbar';
+import './App.css';
 
 function App() {
   return (
@@ -16,7 +16,6 @@ function App() {
             <Route path="/" element={<Login />} />
             <Route path="/Home" element={<Home />} />
             <Route path="/Signup" element={<Signup />} />
-            <Route path="/Booking" element={<Booking />} />
           </Routes>
 
         </div>
---
+++ src/Pages/Home/Card.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function ActionAreaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://img.freepik.com/free-vector/cartoon-school-bus-white-background_1308-132853.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696550400&semt=ais"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
